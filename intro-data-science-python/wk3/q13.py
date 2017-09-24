52c5bd6cb03d089a50d815944e619b5ca4040a7d
Aziz Javed <azizjaved0@gmail.com>
2017-09-24 15:25:10 -0500

    intro python wk4 initial

--- intro-data-science-python/wk3/q13.py
+++ intro-data-science-python/wk3/q13.py
@@ -3,7 +3,7 @@ import q1
 def answer():
     top15 = q1.answer()
     top15['pop'] = top15['Energy Supply'] / top15['Energy Supply per Capita']
-    print(top15.loc['China','pop'])
+    print(top15.loc['China', 'pop'])
     PopEst = top15['pop'].map('{:,}'.format)
     return PopEst
 print(answer())
