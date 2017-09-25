cc9696da97033acf9543d5b2e5352ae566e465b3
Aziz Javed <azizjaved0@gmail.com>
2017-09-24 22:45:28 -0500

    into python wk4 get_list_of_university_towns complete

--- intro-data-science-python/wk4/solution.py
+++ intro-data-science-python/wk4/solution.py
@@ -1,4 +1,43 @@
+import re
 import pandas as pd
 import numpy as np
 from scipy.stats import ttest_ind
 
+states = {'OH': 'Ohio', 'KY': 'Kentucky', 'AS': 'American Samoa', 'NV': 'Nevada', 'WY': 'Wyoming', 'NA': 'National',
+          'AL': 'Alabama', 'MD': 'Maryland', 'AK': 'Alaska', 'UT': 'Utah', 'OR': 'Oregon', 'MT': 'Montana',
+          'IL': 'Illinois', 'TN': 'Tennessee', 'DC': 'District of Columbia', 'VT': 'Vermont', 'ID': 'Idaho',
+          'AR': 'Arkansas', 'ME': 'Maine', 'WA': 'Washington', 'HI': 'Hawaii', 'WI': 'Wisconsin', 'MI': 'Michigan',
+          'IN': 'Indiana', 'NJ': 'New Jersey', 'AZ': 'Arizona', 'GU': 'Guam', 'MS': 'Mississippi', 'PR': 'Puerto Rico',
+          'NC': 'North Carolina', 'TX': 'Texas', 'SD': 'South Dakota', 'MP': 'Northern Mariana Islands', 'IA': 'Iowa',
+          'MO': 'Missouri', 'CT': 'Connecticut', 'WV': 'West Virginia', 'SC': 'South Carolina', 'LA': 'Louisiana',
+          'KS': 'Kansas', 'NY': 'New York', 'NE': 'Nebraska', 'OK': 'Oklahoma', 'FL': 'Florida', 'CA': 'California',
+          'CO': 'Colorado', 'PA': 'Pennsylvania', 'DE': 'Delaware', 'NM': 'New Mexico', 'RI': 'Rhode Island',
+          'MN': 'Minnesota', 'VI': 'Virgin Islands', 'NH': 'New Hampshire', 'MA': 'Massachusetts', 'GA': 'Georgia',
+          'ND': 'North Dakota', 'VA': 'Virginia'}
+
+def isState(line: str):
+    return line.endswith('[edit]')
+
+def cleanState(line: str):
+    return line.replace('[edit]', '')
+
+def cleanCity(line: str):
+    return re.sub(r' \(.+$', '', line)
+
+def get_list_of_university_towns():
+    stateTownsList = []
+
+    with open("university_towns.txt", encoding="utf-8") as file:
+        currentState = None
+
+        for line in file:
+            line = line.strip()
+            if isState(line):
+                currentState = cleanState(line)
+            else:
+                stateTownsList.append((currentState, cleanCity(line)))
+
+    return pd.DataFrame(stateTownsList, columns=["State", "RegionName"])
+
+df = get_list_of_university_towns()
+print(df)
