385b9c071fe0303b42474d0877d956c0eb7181a8
Aziz Javed <azizjaved0@gmail.com>
2017-09-28 23:30:43 -0500

    WIP

--- applied-plotting/wk2/solution.py
+++ applied-plotting/wk2/solution.py
@@ -24,33 +24,46 @@ def leaflet_plot_stations(binsize, hashid):
 
 def highLowsPlots():
     originalTemp = pd.read_csv(fileName + '.csv')
-    originalTemp = originalTemp.where(~originalTemp['Date'].str.endswith('02-29')).dropna().sort_values(by='Date')
-    originalTemp.loc[:, 'Date'] = pd.to_datetime(originalTemp['Date'])
-    originalTemp.set_index('Date', inplace=True)
+    originalTemp = originalTemp \
+        .where(~originalTemp['Date'].str.endswith('02-29')) \
+        .dropna() \
+        .replace({'Date': {r'2015-(.+)': r'1900-\1', r'(?!1900)^.+?-(.+)': r'2015-\1'}}, regex=True) \
+        .groupby(['Element', 'Date']) \
+        .agg({'Data_Value': {'max': np.max, 'min': np.min}})
 
-    temp = originalTemp['2005':'2014']
-    tempMax = temp[temp['Element'] == 'TMAX'].groupby(level=0).max()
-    tempMin = temp[temp['Element'] == 'TMIN'].groupby(level=0).min()
+    originalTemp.index = originalTemp.index.map(lambda t: (t[0], pd.to_datetime(t[1])))
+    maxAllData = originalTemp.loc['TMAX', ('Data_Value', 'max')]
+    minAllData = originalTemp.loc['TMIN', ('Data_Value', 'min')]
+    print(maxAllData)
 
-    temp2015 = originalTemp['2015']
-    temp2015max = pd.concat([temp2015[temp2015['Element'] == 'TMAX'].groupby(level=0).max()]*10)
-    temp2015max['New_Date'] = tempMax.index
-    temp2015max.set_index('New_Date', inplace=True)
-    temp2015min = pd.concat([temp2015[temp2015['Element'] == 'TMIN'].groupby(level=0).min()]*10)
-    temp2015min['New_Date'] = tempMin.index
-    temp2015min.set_index('New_Date', inplace=True)
+    max05_14 = maxAllData['2015']
+    min05_14 = minAllData['2015']
+    max05_14_dates = max05_14.index.values
+    # print(originalTemp)
+    # gotta split the data first, taking 2015 out. 
+    # print(originalTemp)
+    # originalTemp.loc[:, 'Date'] = pd.to_datetime(originalTemp['Date'])
+    # originalTemp.set_index('Date', inplace=True)
+    # temp = originalTemp['2005':'2014']
+    # tempMax = temp[temp['Element'] == 'TMAX'].groupby(level=0).max()
+    # tempMin = temp[temp['Element'] == 'TMIN'].groupby(level=0).min()
 
-    tempMax['Broken_Record_2015'] = temp2015max.where(temp2015max['Data_Value'] > tempMax['Data_Value']).loc[:, 'Data_Value']
-    tempMin['Broken_Record_2015'] = temp2015min.where(temp2015min['Data_Value'] < tempMin['Data_Value']).loc[:, 'Data_Value']
+    # temp2015 = originalTemp['2015']
+    # temp2015max = pd.concat([temp2015[temp2015['Element'] == 'TMAX'].groupby(level=0).max()]*10)
+    # temp2015max['New_Date'] = tempMax.index
+    # temp2015max.set_index('New_Date', inplace=True)
+    # temp2015min = pd.concat([temp2015[temp2015['Element'] == 'TMIN'].groupby(level=0).min()]*10)
+    # temp2015min['New_Date'] = tempMin.index
+    # temp2015min.set_index('New_Date', inplace=True)
 
-    plt.plot(
-        tempMax.index, tempMax['Data_Value'],
-        tempMin.index, tempMin['Data_Value'])
+    # tempMax['Broken_Record_2015'] = temp2015max.where(temp2015max['Data_Value'] > tempMax['Data_Value']).loc[:, 'Data_Value']
+    # tempMin['Broken_Record_2015'] = temp2015min.where(temp2015min['Data_Value'] < tempMin['Data_Value']).loc[:, 'Data_Value']
 
-    plt.gca().fill_between(tempMin.index, tempMax['Data_Value'], tempMin['Data_Value'], facecolor='red', alpha=0.8)
+    plt.plot(max05_14_dates, max05_14.values, max05_14_dates, min05_14.values)
+    plt.gca().fill_between(max05_14_dates, max05_14.values, min05_14.values, facecolor='gray', alpha=0.5)
 
-    plt.scatter(tempMax.dropna().index, tempMax.dropna().loc[:, 'Broken_Record_2015'], color='g', s=10)
-    plt.scatter(tempMin.dropna().index, tempMin.dropna().loc[:, 'Broken_Record_2015'], color='g', s=10)
+    # plt.scatter(tempMax.dropna().index, tempMax.dropna().loc[:, 'Broken_Record_2015'], color='g', s=10)
+    # plt.scatter(tempMin.dropna().index, tempMin.dropna().loc[:, 'Broken_Record_2015'], color='g', s=10)
     plt.show()
 
 highLowsPlots()
