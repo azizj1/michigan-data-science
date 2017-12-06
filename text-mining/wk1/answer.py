0d67a19ab32888a22b5aa394ea81342965ecced5
Aziz Javed <azizjaved0@gmail.com>
2017-12-06 17:35:30 -0600

    text mining wk1 complete

--- text-mining/wk1/answer.py
+++ text-mining/wk1/answer.py
@@ -1,9 +1,7 @@
+from datetime import date
+import re
 import pandas as pd
-
-pd.set_option('display.max_rows', 500)
-pd.set_option('display.max_columns', 500)
-pd.set_option('display.max_colwidth', 250)
-pd.set_option('display.width', 350)
+import numpy as np
 
 def data():
     doc = []
@@ -12,13 +10,34 @@ def data():
             doc.append(line)
     return pd.Series(doc)
 
+def to_date(s):
+    year = s[4]
+    month = 1
+    day = 1
+    if s[2] > 0: # jan, feb, .. converted to 1-12
+        month = s[2]
+        day = max(s[1], s[3], 1) # 1 in case there are no days
+    elif s[1] < 0 or s[3] < 0: # means one of them is a month, the other is no date (day = 1)
+        month = max(s[1], s[3], 1)
+        day = 1
+    else:
+        month = s[1]
+        day = s[3]
+    if re.match(r'^\d\d[\s,]+\d\d\d\d$', s[0]) is not None: # cases where we were getting "16, 1976" or "19, 1988"
+        day = 1
+        month = 1
+    return pd.Series({0: s[0], 1: s[1], 2: s[2], 3: s[3], 4: s[4], 'date': date(year, month, day)})
+
 def date_sorter(df):
-    return df.str.extract(r'\b0?((?:([0-3]?\d)(?:[-/\s]|(?=[A-Z])))?[A-Za-z]?((?:Jan|Feb|Mar|Apr|May|Jun|Jul|Aug|Sep|Oct|Nov|Dec)[a-z,\.]*?[-/\s])?(?:([0-3]?\d)(?:st|nd|th)?,?[-/\s])?(?:s|y)?((?:(?:20|19)\d\d)|(?:(?<=/)\d\d)|(?:(?<=-\d-)\d\d)|(?:(?<=-\d\d-)\d\d)))\b')
+    return df.str.extract(r'\b0?((?:([0-3]?\d)(?:[-/\s]|(?=[A-Z])))?[A-Za-z]?((?:Jan|Feb|Mar|Apr|May|Jun|Jul|Aug|Sep|Oct|Nov|Dec)[a-z,\.]*?[-/\s])?(?:([0-3]?\d)(?:st|nd|th)?,?[-/\s])?(?:s|y)?((?:(?:20|19)\d\d)|(?:(?<=/)\d\d)|(?:(?<=-\d-)\d\d)|(?:(?<=-\d\d-)\d\d)))\b') \
+                .replace({4: {r'^(\d\d)$': r'19\1'}, 2: {r'^.*Jan.*$': 1, r'^.*Feb.*$': 2, r'^.*Mar.*$': 3, r'^.*Apr.*$': 4, r'^.*May.*$': 5, r'^.*Jun.*$': 6, r'^.*Jul.*$': 7, r'^.*Aug.*$': 8, r'^.*Sep.*$': 9, r'^.*Oct.*$': 10, r'^.*Nov.*$': 11, r'^.*Dec.*$': 12}}, regex=True) \
+                .fillna(-1) \
+                .astype({1: np.int32, 2: np.int32, 3: np.int32, 4: np.int32}) \
+                .apply(to_date, axis=1) \
+                .sort_values(by='date')
 
 def execute():
     s = data()
-    df = date_sorter(s)
-    merged = pd.merge(df, pd.DataFrame(s, columns=['Original']), left_index=True, right_index=True)
-    print(merged)
+    return date_sorter(s).reset_index().iloc[:, 0]
 
 execute()
