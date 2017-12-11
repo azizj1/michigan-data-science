9c4e8f57124b4b511198aef359534fd2bce7ecd9
Aziz Javed <azizjaved0@gmail.com>
2017-12-11 09:09:15 -0600

    text-mining wk2 part1 complete

--- text-mining/wk2/part1.py
+++ text-mining/wk2/part1.py
@@ -2,13 +2,15 @@ import re
 import nltk
 from nltk.stem import WordNetLemmatizer
 from nltk.probability import FreqDist
+import numpy as np
 
 def data():
     with open('moby.txt', 'r') as file:
         moby_raw = file.read()
     tokens = nltk.word_tokenize(moby_raw)
     text = nltk.Text(tokens)
-    return tokens, text
+    sentences = nltk.sent_tokenize(moby_raw)
+    return tokens, text, sentences
 
 def num_tokens(tokens):
     return len(tokens) # or len(text)
@@ -28,7 +30,7 @@ def q1(tokens):
 def q2(tokens):
     pattern = re.compile('^[wW]hale$')
     filtered_count = len(list(filter(pattern.match, tokens)))
-    return filtered_count / len(tokens)
+    return filtered_count / len(tokens) * 100
 
 def q3(text):
     freq = FreqDist(text)
@@ -36,7 +38,7 @@ def q3(text):
 
 def q4(text):
     freq = FreqDist(text)
-    return [word for word in freq.keys() if len(word) > 5 and freq[word] > 150]
+    return sorted([word for word in freq.keys() if len(word) > 5 and freq[word] > 150])
 
 def q5(tokens):
     word = max(tokens, key=len)
@@ -48,5 +50,10 @@ def q6_wrong(text):
 
 def q6(text):
     freq = FreqDist(text)
-    return list(filter(lambda x: x[0].isalpha() and x[1] > 2000, freq.most_common()))
+    return list(map(lambda t: (t[1], t[0]), filter(lambda x: x[0].isalpha() and x[1] > 2000, freq.most_common())))
 
+def q7(sentences):
+    return np.mean([len(nltk.word_tokenize(sent)) for sent in sentences])
+
+def q8(tokens):
+    return FreqDist(tag for (word, tag) in nltk.pos_tag(tokens)).most_common()[:5]
